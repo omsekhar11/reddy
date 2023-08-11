cb86127c20395de35feefa69d7783a0f92e736cb
EC2 Default User <ec2-user@ip-172-31-40-54.ap-south-1.compute.internal>
2023-08-11 11:28:51 +0000

    file added

---
+++ apache-maven-3.9.4/reddy/src/main/java/braatch/App.java
@@ -0,0 +1,13 @@
+package braatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
